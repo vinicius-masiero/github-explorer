49af6b1be117f501b2496e6a5a7a3f9c4575b583
Vinícius Masiero <vinicius.masiero@outlook.com>
2020-09-01 21:16:22 -0300

    Updates

---
+++ precache-manifest.4f5faf6a77e15475573363d16137fa87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a4e6cc237098596a704dcd0d1f81f5",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "2810e929c76e05c701bc",
+    "url": "/github-explorer/static/js/2.15c0aa4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.15c0aa4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf3e266c75695f208b9",
+    "url": "/github-explorer/static/js/main.5d0ee9a7.chunk.js"
+  },
+  {
+    "revision": "b76f610d1055037be066",
+    "url": "/github-explorer/static/js/runtime-main.efd11135.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
